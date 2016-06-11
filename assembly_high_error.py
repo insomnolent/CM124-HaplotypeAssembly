c282bfda51e6743499d41f3bda39facd99e4ed45
insomnolent <jbcccsun@gmail.com>
2016-06-11 04:31:19 -0700

    high frequency giving different error rates

--- assembly_high_error.py
+++ assembly_high_error.py
@@ -88,18 +88,6 @@ def compareReads(read, hap, index):
     return result
 
 
-# finds reads from subset that are similar (only 1 error) to read
-def findSimilar(subset):
-    # check each col of each line of subset and see how many cols match
-    # for some reason import Counter doesn't work
-    str = ""
-    freq = []
-    for i in range(0, len(subset)):
-        str += subset[i]
-        str += " "
-    return freq
-
-
 def gettopreads(subset):
     return top2(get_frequency(subset))
 
@@ -154,26 +142,55 @@ def checkInverse(read1, read2):
 # maybe compare it column by column
 def newFiltered(reads, length):
     new = []
+    recentread1 = ""
+    recentread2 = ""
     for i in range(0, length):
         readset1 = takeSubset(reads, i)
         topreads = gettopreads(readset1)
-        if len(topreads) == 1:
-            new.append(topreads[0])
-        elif len(topreads) > 1:
-            read1 = removeDash(topreads[0])
-            read2 = removeDash(topreads[1])
+        # if beginning of read matrix then set recentreads to haplotype beginnings
+        if i == 0:
+            recentread1 = topreads[0]
+            recentread2 = topreads[1]
 
-            if checkInverse(read1, read2):
+        if len(topreads) == 1:
+            if overlap(read1, recentread1, i) or overlap(read1, recentread2, i):
                 new.append(topreads[0])
+                recentread1 = topreads[0]
+        elif len(topreads) > 1:
+            read1 = topreads[0]
+            read2 = topreads[1]
+
+            if checkInverse(removeDash(read1), removeDash(read2)):
+                #if (overlap(recentread1, read1, i) and overlap(recentread2, read2, i)) or (overlap(recentread1, read2, i) and overlap(recentread2, read1, i)):
+                print read1
+                print read2
+                recentread1 = topreads[0]
+                new.append(topreads[0])
+                recentread2 = topreads[1]
                 new.append(topreads[1])
             else:
                 # if the most frequent reads aren't inverses of each other
                 for j in range(0, len(topreads)-1):
                     read1 = topreads[j]
+                    # if overlap(recentread1, read1, i) or overlap(recentread2, read1, i):
+                    #     recentread1 = read1
+                    #     new.append(read1)
+                    #
+
+
                     for k in range(j+1, len(topreads)):
                         read2 = topreads[k]
                         if checkInverse(removeDash(read1), removeDash(read2)):
+                        #     print "does it reach here"
+                        #     print recentread1
+                        #     print 'read1', read1
+                        #     print recentread2
+                        #     print 'read2', read2
+
+                        # if (overlap(recentread1, read1, i) and overlap(recentread2, read2, i)) or (overlap(recentread1, read2, i) and overlap(recentread2, read1, i)):
+                            recentread1 = read1
                             new.append(read1)
+                            recentread2 = read2
                             new.append(read2)
                             break
     return new
@@ -202,11 +219,12 @@ def findStart(read):
     return result
 
 
-# test if 2nd read is completely overlapped by 1st read
-def overlap(first, second):
+# test if a read at that index is overlapped by the read above it
+def overlap(first, second, index):
     result = True
-    for i in range(0, len(first)):
-        if i < len(second) and first[i] != second[i]:
+    end = index + len(removeDash(first))-1
+    for i in range(index, end):
+        if first[i] != second[i]:
             result = False
     return result
 
@@ -230,7 +248,7 @@ def percentOverlap(hap1, hap2):
         if c == hap2[track]:
             percent += 1
         track += 1
-    return float(percent)/float(len(hap1)) * 100
+    return float(percent)/float(len(hap2)) * 100
 
 
 # read_matrix = read_input('./medium/example/input/reads_high_error.txt')
@@ -244,9 +262,9 @@ length = len(read_matrix[0])
 # make new matrix with longest reads from each subset
 new_matrix = newFiltered(read_matrix, length)
 
-print 'new_matrix '
-for c in new_matrix[0:500]:
-    print c
+# print 'new_matrix '
+# for c in new_matrix[0:500]:
+#     print c
 
 # get rid of empty lines in new_matrix
 new_matrix = [i for i in new_matrix if i != '']
@@ -266,12 +284,8 @@ if new_matrix[1][0] == '-':
 else:
     hap2 = removeDash(new_matrix[1])
 
-
-# list comprehensions
-# somelist[:] = [x for x in somelist if not determine(x)]
 for i in range(2, len(new_matrix)):
     read1 = new_matrix[i]
-    # print read1
     ind = findStart(read1)
     compare1 = compareReads(read1, hap1, ind)
     compare2 = compareReads(read1, hap2, ind)
@@ -290,8 +304,11 @@ for i in range(0, len(hap2)):
     sys.stdout.write(hap2[i])
 
 print "\n"
+# Final_2, high error data - currently 50.4% and 50.6%
 # answer1 = "0100101000011100011000010000101101010000111111011101111110000100001000000011001100100001001001010111011110001100101001100111101100001010101000000100011101101101010010011011111100000101000101111111010011101010001000001110100011110111000111111110111111001101000110111101011011111100101101101011011100110110110111101011110110101010101111011011000100010010100101111100000111011010010001111101010000011101100101101010001110011101101001100000100010000000011100011011000000011000010111001110111001000000110100111111111110011001000010010011001110111011000000110000110111010001110110001000011101101110001111110010111011001100110001110000111110000111110010000100101110011001110111110010101100011001111101000110110100010111001010111011011101011000011110111111111101001100100011011100101010001001000100001011001110111000101001000100000110100100100110000000101100110001011011010100000000110110111001100100111011010110110100001100010010111110100011011111101001100000101010110100010010000011001110111010101101101001"
 # answer2 = "1011010111100011100111101111010010101111000000100010000001111011110111111100110011011110110110101000100001110011010110011000010011110101010111111011100010010010101101100100000011111010111010000000101100010101110111110001011100001000111000000001000000110010111001000010100100000011010010010100100011001001001000010100001001010101010000100100111011101101011010000011111000100101101110000010101111100010011010010101110001100010010110011111011101111111100011100100111111100111101000110001000110111111001011000000000001100110111101101100110001000100111111001111001000101110001001110111100010010001110000001101000100110011001110001111000001111000001101111011010001100110001000001101010011100110000010111001001011101000110101000100100010100111100001000000000010110011011100100011010101110110111011110100110001000111010110111011111001011011011001111111010011001110100100101011111111001001000110011011000100101001001011110011101101000001011100100000010110011111010101001011101101111100110001000101010010010110"
+
+# data 3, easy high error - currently 87%
 answer1 = "0101111100111101101111010010001000000001100101111100101011000111000000000100010110011101111000100001"
 answer2 = "1010000011000010010000101101110111111110011010000011010100111000111111111011101001100010000111011110"
 
